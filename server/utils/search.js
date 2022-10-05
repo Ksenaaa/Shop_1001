a140be23e8b40d49fddbdf4a3ff1562a2b7e772a
Ksena <ksena@ip-192-168-1-5.ec2.internal>
2022-10-05 12:52:38 +0300

    Client folder

--- server/utils/search.js
+++
@@ -1,8 +0,0 @@
-const searchBooksByNameAndAuthor = (allBooks, text) => {
-    return allBooks.filter(book => {
-        return book.bookName.toLowerCase().includes(text.toLowerCase().trim()) 
-        || book.author.toLowerCase().includes(text.toLowerCase().trim())
-    })
-}
-
-module.exports = searchBooksByNameAndAuthor
